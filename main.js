f972a019af4260870372092fcb86821e4a94fc3e
Maria Mendoza Zafra <mmendozazafra@gmail.com>
2022-12-11 22:30:47 -0500

    Calc area Triangle
    Se permite el ingreso de datos para Triangulo y se realizan dos operaciones: perímetro y área.

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 //Declaración de variables
 
-let ladoC = document.querySelector('.ladoSquare');
-let perimetroC = document.querySelector('.perimetro-cuadrado');
-let areaC = document.querySelector('.area-cuadrado');
-let valorC = document.querySelectorAll('.valor');
+//Cuadrado
+const ladoC = document.querySelector('.ladoSquare');
+const perimetroC = document.querySelector('.perimetro-cuadrado');
+const areaC = document.querySelector('.area-cuadrado');
+const valorC = document.querySelectorAll('.valor');
 
+//Triangulo
+const lado1 = document.querySelector('.lado1Triangle');
+const lado2 = document.querySelector('.lado2Triangle');
+const lado3 = document.querySelector('.lado3Triangle');
+const altura = document.querySelector('.alturaTriangle');
+const areaT = document.querySelector('.area-triangulo');
+const perimetroT = document.querySelector('.perimetro-triangulo');
+
+
+//Funciones
+
+//Cuadrado
 ladoC.addEventListener('change',()=>{
     if (ladoC.value > 0){
         perimetroC.innerHTML = "El perímetro del cuadrado es: " + ladoC.value * 4;
@@ -17,6 +30,29 @@ ladoC.addEventListener('change',()=>{
     } else{
         perimetroC.innerHTML = "El lado no puede ser 0";
         areaC.innerHTML =  "El lado no puede ser 0";
+    }
+
+})
+
+//Triangulo
+lado3.addEventListener('change',()=>{
+
+    if(lado1.value > 0 || lado2.value > 0 || lado3.value > 0){
+
+        let v1 = Number(lado1.value);
+        let v2 = Number(lado2.value);
+        let v3 = Number(lado3.value);
+        let alt = Number(altura.value);
+
+        let suma = v1 + v2 + v3;
+        let area = (v3 * alt) / 2;
+
+        perimetroT.innerHTML = "El perímetro del triangulo es: " + suma;
+        areaT.innerHTML =  "El área del triangulo es: " + area;
+       
+    }else{
+        perimetroT.innerHTML = "El lado no puede ser 0";
+        areaT.innerHTML =  "El lado no puede ser 0";
     }
 
 })
